9fa547ae7d3f28ec92bfd38dcc2d2194729f1b6a
iqbolshoh <iilhomjonov777@gmail.com>
2026-06-13 12:15:37 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// ========== MENU DATA ==========
+const menuItems = [
+    { id: 1, name: "Osh", category: "lunch", price: 35000, desc: "An'anaviy o'zbek palovi", image: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=300&h=200&fit=crop", rating: 4.8 },
+    { id: 2, name: "Manti", category: "lunch", price } ]
